6c1ccd65b9f19f7e3bc2c8ed60260a96774b6498
junhyukseo <junhyuk.seo@devsisters.com>
2026-04-19 20:54:46 +0900

    feat: add sound player for event-triggered audio

---
+++ Sources/ClaudeSpeaki/SoundPlayer.swift
@@ -0,0 +1,25 @@
+import AppKit
+
+class SoundPlayer {
+    private var sounds: [EventType: NSSound] = [:]
+    var volume: Float = 1.0
+
+    func loadSounds(from directory: URL) {
+        let extensions = ["wav", "mp3", "aiff", "m4a"]
+        for eventType in EventType.allCases {
+            for ext in extensions {
+                let url = directory.appendingPathComponent("\(eventType.rawValue).\(ext)")
+                if let sound = NSSound(contentsOf: url, byReference: false) {
+                    sounds[eventType] = sound
+                    break
+                }
+            }
+        }
+    }
+
+    func play(for event: EventType) {
+        guard let sound = sounds[event] else { return }
+        sound.volume = volume
+        sound.play()
+    }
+}
